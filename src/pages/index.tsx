c0cf4b8af1e64a21c365534aa31ae6e6266bc461
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-11-20 20:19:54 -0300

    homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,73 @@
 import * as C from '../styles/home'
-
+import Head from 'next/head'
+import Image from 'next/image'
+import techImage from '../../public/images/techs.svg'
 export default function Home() {
   return (
-    <C.container>
-      <p>Página home</p>
-    </C.container>
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia - Sujeito Programador</title>
+      </Head>
+      <C.container>
+        <div className="containerHeader">
+          <section className="ctaText">
+            <h1>Levando você ao próximo nível!</h1>
+            <span>
+              Uma plataforma com cursos que vão do zero ate o profissional na
+              pratica, direto ao ponto aplicando o que usamos no mercado de
+              trabalho.
+            </span>
+            <a>
+              <button>COMEÇAR AGORA!</button>
+            </a>
+          </section>
+          <img
+            src="/images/banner-conteudos.png"
+            alt="Conteúdos Sujeito Programador"
+          />
+        </div>
+        <C.divisor></C.divisor>
+
+        <C.sectionContent>
+          <section>
+            <h2>Aprenda a criar aplicativos para Android e IOS</h2>
+            <span>
+              Você vai descobrir o jeito mais moderno de desenvolver apps,
+              nativos para IOS e Androis, construindo do zero até a aplicação.
+            </span>
+          </section>
+          <img
+            src="/images/financasApp.png"
+            alt="conteúdos desenvolvimento de apps"
+          />
+        </C.sectionContent>
+
+        <C.divisor></C.divisor>
+
+        <C.sectionContent>
+          <img
+            src="/images/webDev.png"
+            alt="conteúdos desenvolvimento de aplicações web"
+          />
+          <section>
+            <h2>Aprenda a criar sistemas web</h2>
+            <span>
+              Criar sitemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.
+            </span>
+          </section>
+        </C.sectionContent>
+
+        <C.nextLevelContent>
+          <Image quality={100} src={ techImage } alt="tecnologias"/>
+          <h2>Mais de <span className='alunos'>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
+          <span>
+            E você vai perder a chance de evoluir de uma vez por todas?
+          </span>
+          <a href="#">
+            <button>ACESSAR TURMA!</button>
+          </a>
+        </C.nextLevelContent>
+      </C.container>
+    </>
   )
 }
--- src/styles/home.ts
+++ src/styles/home.ts
@@ -1,5 +1,161 @@
 import styled from 'styled-components'
 
 export const container = styled.div`
+display:block;
+
+.containerHeader{
+  min-height: calc(100vh - 5rem);
+  max-width:1120px;
+  padding-inline:2rem;
+  margin: 0 auto;
+  display:flex;
+  align-items:center;
+  justify-content:space-between;
+  
+  .ctaText{
+    max-width:600px;
+    h1{
+      font-size:3.5rem;
+      line-height:4.5rem;
+      font-weight:900;
+      margin-top:2.5rem;
+      color:var(--white)
+    }
+    span{
+      font-size:1rem;
+      line-height: 2.25rem;
+      color:var(--white);
+    }
+    button{
+      display:block;
+      background-color:var(--blue-900);
+      border:none;
+      padding:1rem 1.8rem;
+      margin-top:2.5rem;
+      border-radius:7px;
+      color:var(--white);
+      font-weight:bold;
+    }
+  }
+  img{
+    max-width:640px;
+  }
+}
+@media (max-width:1095px){
+  .containerHeader{
+    padding:0 2rem;
+
+    .ctaText h1{
+      font-size:3rem;
+    }
+
+    img{
+      max-width:600px;
+    }
+  }
+}
+
+@media (max-width:950px){
+  .containerHeader{
+    flex-direction:column;
+
+    button{
+      width:100%;
+      display:flex;
+      align-items:center;
+      justify:content:center;
+    }
+    img{
+      max-width:500px;
+    }
+  }
+}
+`
+export const divisor = styled.hr`
+width:100%,
+bottom:0;
+height:1px;
+border-color:var(--gray-200);
+`
+
+export const sectionContent = styled.div`
+  margin: 5.5rem auto;
+  max-width: 1120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  section {
+    max-width: 670px;
+  }
+
+  img {
+    max-width: 550px;
+  }
+  h2 {
+    font-size: 2.2rem;
+    font-weight: 900;
+    margin-top: 2.2rem;
+    line-height: 4.5rem;
+    color: var(--white);
+  }
+
+  span {
+    font-size: 1rem;
+    line-height: 2.25rem;
+    color: var(--white);
+  }
+  @media (max-width: 1095px) {
+    padding: 0 2rem;
+    h2{
+      font-size:2.3rem
+    }
+  }
+  @media (max-width:950px){
+    flex-direction:column;
+    img{
+      margin-top:2rem;
+      max-width:400px;
+    }
+  }
+`
+
+export const nextLevelContent = styled.div`
+  margin: 6rem auto;
+  max-width: 1120px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  h2 {
+    font-size: 2.8rem;
+    line-height: 4.5rem;
+    font-weight: 900;
+    color: var(--white);
+  }
+  .alunos {
+    color: var(--blue-900);
+    font-size: 2.8rem;
+    line-height: 4.5rem;
+    font-weight: 900;
+  }
+  span {
+    font-size: 1rem;
+    line-height: 2.25rem;
+    color: var(--white);
+  }
+  button {
+    background-color: var(--blue-900);
+    border: none;
+    padding: 1rem 1.8rem;
+    margin-top: 2.5rem;
+    border-radius: 6px;
+    color: var(--white);
+    font-weight: bold;
+  }
+  @media (max-width: 1095px) {
+    padding: 0 2rem;
+  }
 
 `
